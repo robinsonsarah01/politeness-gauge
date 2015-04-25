4b0bed8722c9507b672b21bb57aed5eb1eba0e7c
Sarah Robinson <robin26s@mtholyoke.edu>
2015-04-24 22:03:29 -0400

    Update measurements to re-adjust overall score

--- measurements.py
+++ measurements.py
@@ -37,7 +37,10 @@ def get_scores(input_text):
     
     counts["emoticons"] = emoticons.get_emoticon_count(input_text)
     
-    counts["input_len"] = len(tokens)
+    print input_text.count(".")
+    counts["input_len"] = len(tokens) - input_text.count(".")
+    
+    print counts["politeness"], counts["input_len"], "--", float(counts["politeness"]) / counts["input_len"]
     
     return counts
     
@@ -46,9 +49,11 @@ def get_overall_score():
     """
     Based on current counts, calculate an overall politeness score.
     """
-    # TODO make this relative to the size of the input
     score = 50
-    score += counts["politeness"] * 4
+    if counts["input_len"] != 0:
+        # politeness score on n-grams is relative to input length
+        # out of 75 to make the top politeness score something like 50 -- then top overall score ~100
+        score += ( float(counts["politeness"]) / counts["input_len"] ) * 75
     score -= counts["curses"] * 10
     score -= counts["emoticons"] * 4
     return score
@@ -58,6 +63,7 @@ if __name__ == "__main__":
     input = "Thank you for contacting me! I'm generally free in the mornings before 11 and in the afternoons after 4 on weekdays; is there any time that would be most convenient to meet to discuss the job? thank you,"
     print get_scores(input)
     print get_overall_score()
+    print
     input = "wtf prof i told you i needed that damn recommendation like today. fuck!!! >:O dude like wtf"
     print get_scores(input)
     print get_overall_score()
